5a02505c1b0a518a23b80e52d1e1176314778987
Edward Abrahamian <Abrahamian.Edward@hotmail.com>
2024-03-24 18:23:57 +0100

    ajout import export supplémentaire dans le fichier admin.py

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -4,15 +4,30 @@ from django.contrib import admin
 
 from .models import *
 
-#EA Import CSV
+#Edward Abrahamian - for import/export CSV
 from import_export.admin import ImportExportModelAdmin
 
 
 admin.site.register(Artist, ImportExportModelAdmin)
 class ArtistAdmin(admin.ModelAdmin):
-
     pass
 
 admin.site.register(Show, ImportExportModelAdmin)
 class ShowAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Type, ImportExportModelAdmin)
+class TypeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Role, ImportExportModelAdmin)
+class RoleAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Location, ImportExportModelAdmin)
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Locality, ImportExportModelAdmin)
+class LocalityAdmin(admin.ModelAdmin):
     pass
