70d7e0d6f82c901527448df71f7b67fc127f4c5b
Billyma21 <Bilal.maayoud@live.fr>
2024-03-26 02:30:22 +0100

    View, urls, hostorique commande + détaille

--- catalogue/views/payment.py
+++ catalogue/views/payment.py
@@ -1,54 +1,9 @@
 #<!-- Bilal ma - Views paiement -->
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from catalogue.models import Cart, Order
 from catalogue.forms import PaymentForm
 from django.contrib.auth.decorators import login_required
-
-# def make_payment(request):
-#     if request.method == 'POST':
-#         form = PaymentForm(request.POST)
-#         if form.is_valid():
-#             payment_method = form.cleaned_data['payment_method']
-#             # Récupérer les données du panier depuis la session
-#             cart_items = request.session.get('cart', {})
-#             total_price = sum(item['quantity'] * item['price'] for item in cart_items.values())
-#             # Créer une commande avec les détails du paiement
-#             order = Order.objects.create(
-#                 user=request.user,
-#                 total_amount=total_price,
-#                 payment_method=payment_method,
-#                 payment_confirmed=False
-#             )
-#             # Sauvegarder la commande dans la base de données
-#             order.save()
-
-#             # Réinitialiser le panier après le paiement
-#             request.session['cart'] = {}
-
-#             # Rediriger vers la page de succès de paiement
-#             return redirect('payment_success')
-#     else:
-#         form = PaymentForm()
-
-#     # Afficher le formulaire de paiement avec les données du panier
-#     cart_items = request.session.get('cart', {})
-#     total_price = sum(item['quantity'] * item['price'] for item in cart_items.values())
-#     return render(request, 'cart/payment_information.html', {'form': form, 'cart_items': cart_items, 'total_price': total_price})
-
-# def payment_success(request):
-#     # Afficher la page de succès de paiement avec un récapitulatif des informations de la commande
-#     last_order = Order.objects.last()
-#     return render(request, 'payment_success.html', {'order': last_order})
-
-
-# def checkout_summary(request):
-#     cart = Cart(request)  # Initialise le panier
-#     total_price = cart.get_total_price()  # Récupère le prix total
-#     cart_items = cart.cart.values()  # Récupère les articles du panier
-
-#     return render(request, 'cart/checkout_summary.html', {'cart_items': cart_items, 'total_price': total_price})
-
 
 @login_required
 def make_payment(request):
@@ -58,11 +13,16 @@ def make_payment(request):
             payment_method = form.cleaned_data['payment_method']
             cart_items = request.session.get('cart', {})
             total_price = sum(item['quantity'] * item['price'] for item in cart_items.values())
+            
+            # Avec les articles commandés
+            order_items = [{'title': item['title'], 'quantity': item['quantity'], 'price': item['price'], 'total_price': item['quantity'] * item['price']} for item in cart_items.values()]
+            
             order = Order.objects.create(
                 user=request.user,
                 total_amount=total_price,
                 payment_method=payment_method,
-                payment_confirmed=False
+                payment_confirmed=False,
+                items=order_items
             )
             order.save()
             request.session['cart'] = {}
@@ -73,17 +33,27 @@ def make_payment(request):
     cart_items = request.session.get('cart', {})
     total_price = sum(item['quantity'] * item['price'] for item in cart_items.values())
     return render(request, 'cart/payment_information.html', {'form': form, 'cart_items': cart_items, 'total_price': total_price})
+# Hostirque de commande & détaille de celle ci
+@login_required
+def order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id, user=request.user)
+    return render(request, 'cart/order_detail.html', {'order': order})
+
+
+@login_required
+def order_history(request):
+    # Récupérer l'historique des commandes de l'utilisateur connecté
+    orders = Order.objects.filter(user=request.user).order_by('-order_date')
+    return render(request, 'cart/order_history.html', {'orders': orders})
+
 
 @login_required
 def payment_success(request):
     last_order = Order.objects.last()
     return render(request, 'cart/payment_success.html', {'order': last_order})
 
-from catalogue.models import Cart
-
 def checkout_summary(request):
     cart = Cart(request)
     cart_items = request.session.get('cart', {})
-  # Utilisation de la méthode get_items() pour obtenir les articles du panier
     total_price = sum(item['quantity'] * item['price'] for item in cart_items.values())
     return render(request, 'cart/checkout_summary.html', {'cart_items': cart_items, 'total_price': total_price})
